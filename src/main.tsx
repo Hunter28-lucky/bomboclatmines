0c9cdb9b1595af0c139596cf2fa8287ba0a282ce
Hunter28-lucky <krrishyogi18@gmail.com>
2025-08-10 23:39:38 +0530

    Fix import path for AdminPage in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import AdminPage from './goswami';
+import AdminPage from './pages/goswami';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
